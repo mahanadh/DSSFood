92645fcd6807a3db5c8224e96cfac46661a10650
mahanadh <mahan2054@gmail.com>
2018-08-29 15:35:09 +0545

    Initial commit

---
+++ forgot_password.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mahan
+ * Date: 8/28/2018
+ * Time: 7:12 PM
+ */
+?>
+<html>
+<head><title>Password Reset</title></head>
+<body>
+<form>
+    <p>Enter Your Email:</p>
+    <br>
+    <input type="email" name="email"><br><br>
+    <input type="submit" name="submit">
+</form>
+</body>
+</html>
