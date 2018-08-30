dd4236b6b9e207a651c26bace3594482209360d5
PrashantSatyal517 <prashant.satyal@deerwalk.edu.np>
2018-08-30 13:41:03 +0545

    Add files via upload

---
+++ DSSfood-1/login_page.php
@@ -0,0 +1,51 @@
+
+
+<html>
+<head><title>Login Page</title></head>
+<body>
+<form action="" method="post">
+    <p>Email:</p>
+    <input type="email" name="email"><br>
+    <p>Password:</p>
+    <input type="password" name="password"><br><br>
+    <input type="submit" name="submit"><br><br><br>
+    <a href="register.html"> Register</a><br><br>
+    <a href="reset_pass.php">Forgot Password ? </a>
+</form>
+</body>
+</html>
+
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mahan
+ * Date: 8/28/2018
+ * Time: 5:54 PM
+ */
+
+session_start();
+
+if(isset($_POST['submit'])){
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $query = "select * from users_dss where email = '$email' && password = '$password'";
+    $db_host="localhost";  $db_user="root";  $db_password="";  $db="dss";
+    $con = mysqli_connect($db_host,$db_user,$db_password,$db);
+    $result = mysqli_query ( $con , $query);
+    $count=mysqli_num_rows($result);
+
+    if($count == 1){
+        $_SESSION["LoggedIn"] = TRUE;
+        header('Location: ' . 'user_dashboard.php');
+    }else{
+
+        echo 'Invalid Details';
+    }
+}
+else
+{
+    echo 'Enter the required fields';
+}
+?>
---
+++ DSSfood-1/user_dashboard.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mahan
+ * Date: 8/29/2018
+ * Time: 2:24 PM
+ */
+echo 'helloooo';
+?>
